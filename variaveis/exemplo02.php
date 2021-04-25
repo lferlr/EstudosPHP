90ff7465a010c92b504466b4839f2edcbb6736fb
Lucas Rodrigues <rodrigues14set@hotmail.com>
2021-04-25 00:15:01 -0300

    Initial commit + Variables

---
+++ variaveis/exemplo02.php
@@ -0,0 +1,24 @@
+<?php
+$nome = "Lucas";
+$site = 'www.hcode.com.br';
+
+$ano = 1990;
+$salario = 5500.99;
+$bloqueado = false;
+//////////////////////////////////////////////
+
+$frutas = array("Abacaxi", "Laranjas", "Manga");
+
+//echo $frutas[2];
+
+$nascimento = new DateTime();
+
+//var_dump($nascimento);
+//////////////////////////////////////////////
+$arquivo = fopen("exemplo02.php", "r");
+
+//var_dump($arquivo);
+
+$nulo = NULL;
+
+?>
---
+++ variaveis/exemplo04.php
@@ -0,0 +1,13 @@
+<?php
+$nome = (int)$_GET["a"];
+$nome2 = (int)$_GET["b"];
+
+/*var_dump($nome);
+echo "<br>";
+var_dump($nome2);*/
+
+$ip = $_SERVER["REMOTE_ADDR"];
+
+echo $ip;
+
+?>
