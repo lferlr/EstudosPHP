c0bd2062c2ebb964cac96e83b2151fec8961ce80
Lucas Rodrigues <rodrigues14set@hotmail.com>
2021-05-13 23:42:37 -0300

    If com PHP

---
+++ if/exemplo01.php
@@ -0,0 +1,22 @@
+<?php
+
+$qualASuaIdade = 13;
+
+$idadeCrianca = 12;
+$idadeMaior = 18;
+$idadeMelhor = 65;
+
+if ( $qualASuaIdade < $idadeCrianca ) {
+  echo "Criança";
+} else if ($qualASuaIdade < $idadeMaior) {
+  echo "Adolecente";
+} else if ($qualASuaIdade < $idadeMelhor) {
+  echo "Adulto";
+} else {
+  echo "Idoso";
+}
+
+echo "<br>";
+
+echo ($qualASuaIdade < $idadeMaior)?"Menor de Idade":"Maior de Idade";
+?>
