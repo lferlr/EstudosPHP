b472f2e0a65bba0f6b76109cbb6ccbe9f68beadf
Lucas Rodrigues <rodrigues14set@hotmail.com>
2021-08-29 00:21:30 -0300

    Final NameSpace

--- namespace/index.php
+++ namespace/index.php
@@ -2,10 +2,15 @@
 
 require_once("config.php");
 
+use Cliente\Cadastro;
+
 $cad = new Cadastro();
+
 $cad->setNome("Lucas Rodrigues");
 $cad->setEmail("lucasflr@gmail.com");
 $cad->setSenha("Lucaslindo");
+
+$cad->registrarVenda();
 
 echo $cad;
 
