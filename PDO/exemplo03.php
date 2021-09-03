2bcf683fe4340d645493c1fadabd33aa45a0a996
Lucas Rodrigues <rodrigues14set@hotmail.com>
2021-09-02 22:39:35 -0300

    PDO Update with PHP + MySQL

--- PDO/exemplo03.php
+++ PDO/exemplo03.php
@@ -2,16 +2,18 @@
 
 $conn = new PDO("mysql:dbname=sakila;host=localhost", "root", "123456");
 
-$stmt = $conn->prepare("INSERT INTO sakila (first_name, last_name) VALUES (:FIRST_NAME, :LAST_NAME)");
+$stmt = $conn->prepare("UPDATE actor SET first_name = :FIRST_NAME, last_name = :LAST_NAME WHERE actor_id = :ID");
 
 $name = "Lucas";
-$finalname = "Rodrigues";
+$finalname = "Fernandes";
+$id = 1;
 
 $stmt->bindParam(":FIRST_NAME", $name);
 $stmt->bindParam(":LAST_NAME", $finalname);
+$stmt->bindParam(":ID", $id);
 
 $stmt->execute();
 
-echo "Inserido com sucesso!";
+echo "Dados alterados com sucesso!!";
 
 ?>
